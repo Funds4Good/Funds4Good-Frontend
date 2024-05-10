c43d99480367fbeb3e392d7e6e988f8b72aad2c4
Tech-Shreyansh <shreyanshagarwal.16.2022@gmail.com>
2024-05-11 02:50:53 +0530

    implemented transaction

--- src/app/wallet/page.tsx
+++ src/app/wallet/page.tsx
@@ -2,13 +2,15 @@
 
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import Navbar from "@/components/Navbar";
+import Address from "../../components/address";
 
 export default function Home() {
   return (
     <div className="grid grid-cols-12">
       <div className="col-span-2"><Navbar page="Wallet" /></div>
       <div className="col-span-10 border hover:border-slate-900 rounded">
-        <WalletMultiButton style={{}} />
+        <WalletMultiButton />
+        <Address /> 
       </div>
     </div>
   );
