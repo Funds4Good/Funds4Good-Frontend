e585dd2b3d45ebc072e3f7a69771767bc2facd8e
Tech-Shreyansh <shreyanshagarwal.16.2022@gmail.com>
2024-05-10 16:13:22 +0530

    add : signin api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Stories from "@/components/Stories";
 export default function Home() {
   return (
     <div className="grid grid-cols-12">
-      <div className="col-span-2"><Navbar /></div>
+      <div className="col-span-2"><Navbar page="Home" /></div>
       <div className="col-span-7"><Post /></div>
       <div className="col-span-3"><Stories /></div>
     </div>
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,7 +1,33 @@
+"use client"
+
 import { Button } from "@/components/ui/Button";
 import { Lock, Mail, User } from "lucide-react";
-
+import { useState } from "react";
+import axios from 'axios'
+import BaseUrl from "@/BaseUrl";
 export default function SignIn() {
+
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [loading, setLoading] = useState(false);
+    function handleSubmit() {
+        if (email && password) {
+            setLoading(true);
+            BaseUrl.post("https://funds4good.pranavbisaria.live/api/auth/login", {
+                email: email,
+                password: password
+            })
+            .then((res)=>{
+                setLoading(true);
+                console.log(res)
+            })
+            .catch((err)=>{
+                setLoading(true);
+                console.log(err)
+            })
+        }
+    }
+
     return (
         <div className="bg-[#F0F4F3] h-[100vh] flex items-center">
             <div className="bg-white rounded-xl w-4/5 mx-auto h-[75vh]">
@@ -16,13 +42,13 @@ export default function SignIn() {
                         <div className="flex flex-col my-8 placeholder-[#9A9A9A] gap-6">
                             <div className="flex mx-auto items-center w-full justify-center">
                                 <div className="p-4 bg-[#F4F8F5] text-[#9A9A9A]"><Mail /></div>
-                                <input className="p-4 bg-[#F4F8F5] outline-none w-1/2" placeholder="Email" />
+                                <input onChange={(e) => setEmail(e.target.value)} className="p-4 bg-[#F4F8F5] outline-none w-1/2" placeholder="Email" />
                             </div>
                             <div className="flex mx-auto items-center w-full justify-center">
                                 <div className="p-4 bg-[#F4F8F5] text-[#9A9A9A]"><Lock /></div>
-                                <input className="p-4 bg-[#F4F8F5] outline-none w-1/2" placeholder="Password" />
+                                <input onChange={(e) => setPassword(e.target.value)} className="p-4 bg-[#F4F8F5] outline-none w-1/2" placeholder="Password" />
                             </div>
-                            <Button className="bg-[#5047E5] text-white w-1/2 p-4 mx-auto rounded-full">Sign In</Button>
+                            <Button onClick={handleSubmit} className="bg-[#5047E5] text-white w-1/2 p-4 mx-auto rounded-full">Sign In</Button>
                         </div>
                     </div>
                 </div>
